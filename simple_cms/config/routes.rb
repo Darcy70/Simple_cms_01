7ff39b8caed27f59a19bf78ae0e661c3a22bc3c3
Darcy70 <darcy0721@gmail.com>
2017-05-04 08:59:44 +0800

    Routing experiment

--- simple_cms/config/routes.rb
+++ simple_cms/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  root 'demo#index'
+
+  get 'demo/index'
+  # match "demo/index", :to => "demo#index", :via => :get
+
+  # default routes may go away
+  # get ':controller(/:action(/:id))'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
